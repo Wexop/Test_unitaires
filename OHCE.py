c6a2295b64e016b72b22e2b1059de25ac2e93f6f
Wexop <mattheurtin@gmail.com>
2023-12-05 10:54:57 +0100

    version fr

--- OHCE.py
+++ OHCE.py
@@ -3,12 +3,36 @@ import time
 hour = time.localtime().tm_hour
 
 time = 'jour'
-if time > 18:
+if hour > 18:
     time = 'night'
 
-if time == 'jour':
-    print('Bonjour !')
-elif time == 'night':
-    print('Bonsoir !')
+
+def getHello():
+    if time == 'jour':
+        return 'Bonjour !'
+    elif time == 'night':
+        return 'Bonsoir !'
 
 
+def getGoodBye():
+    if time == 'jour':
+        return 'Bonne journée !'
+    elif time == 'night':
+        return 'Bonne soirée !'
+
+
+print(getHello())
+
+
+def est_palindrome(mot):
+    mot = mot.lower()
+    mot = mot.replace(" ", "")
+    return mot == mot[::-1]
+
+
+sentence = input('Ecrivez quelque chose : ')
+print(sentence)
+if (est_palindrome(sentence)):
+    print('Bien dit !')
+
+print(getGoodBye())
