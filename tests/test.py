22a115f3fde004e0a66f6cc25198b7480e54c780
Wexop <mattheurtin@gmail.com>
2024-01-30 14:29:55 +0100

    every tests

--- tests/test.py
+++ tests/test.py
@@ -1,9 +1,13 @@
+import os
 import unittest
 
-from src.OHCE import getHello, getGoodBye, printPalindrome
+from src.langueAnglaise import LangueAnglaise
+from src.langueFrançaise import LangueFrançaise
 from tests.utilities.detecteutPalindromeBuilder import DétecteurPalindromeBuilder
+from tests.utilities.langueSpy import LangueSpy
 
 testNonPalindrome = ["test", "clip"]
+
 
 class TestMiroir(unittest.TestCase):
 
@@ -16,20 +20,49 @@ class TestMiroir(unittest.TestCase):
                 attendu = chaîne[::-1]
                 self.assertIn(attendu, résultat)
 
+    def test_félicitations(self):
+        cas = [[LangueFrançaise(), 'Bien dit !'], [LangueAnglaise(), 'Well said !']]
+
+        for paramètres in cas:
+            with (self.subTest(paramètres[0])):
+                langue = paramètres[0]
+                palindrome = 'radar'
+
+                résultat = (DétecteurPalindromeBuilder()
+                            .ayantPourLangue(langue)
+                            .build()
+                            .détecter(palindrome))
+
+                félicitations = paramètres[1]
+                attendu = palindrome + os.linesep + félicitations
+                self.assertIn(attendu, résultat)
+
+    def test_absence_bien_dit(self):
+        for chaîne in testNonPalindrome:
+            with self.subTest(chaîne):
+                langue = LangueSpy()
+                résultat = DétecteurPalindromeBuilder().ayantPourLangue(langue).build().détecter(chaîne)
+
+                self.assertFalse(langue.félicitationsConsultées())
+
     def test_bonjour(self):
-        result = getHello()
-        sortie_attendue = ["Bonjour !", "Hello !", "Good evening !", "Bonsoir !"]
-        self.assertIn(result, sortie_attendue)
 
-    def test_aurevoir(self):
-        result = getGoodBye()
-        sortie_attendue = ["Bonne journée !", "Bonne soirée !", "Have a good evening !", "Have a good day !"]
-        self.assertIn(result, sortie_attendue)
+        chaîne = 'test'
 
-    def test_bien_dit(self):
-        result = printPalindrome('kayak')
-        sortie_attendue = ["Bien dit !", "Well said !"]
-        self.assertIn(result, sortie_attendue)
+        résultat = DétecteurPalindromeBuilder().build().détecter(chaîne)
+
+        premiere_ligne = résultat.split(os.linesep)[0]
+        self.assertEqual('Bonjour', premiere_ligne)
+
+    def test_au_revoir(self):
+
+        chaîne = 'test'
+
+        résultat = DétecteurPalindromeBuilder().build().détecter(chaîne)
+
+        lignes = résultat.split(os.linesep)
+        dernière_ligne = lignes[-1]
+        self.assertEqual('Au revoir', dernière_ligne)
 
 
 if __name__ == '__main__':
