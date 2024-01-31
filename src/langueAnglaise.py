333ac94875b2a053bde108c4e051579a891babd6
Wexop <mattheurtin@gmail.com>
2024-01-31 11:18:52 +0100

    ajout moment de la journée

--- src/langueAnglaise.py
+++ src/langueAnglaise.py
@@ -1,8 +1,20 @@
+from momentDeLaJournée import MomentDeLaJournée
+
+
 class LangueAnglaise:
     def féliciter(self):
         return "Well said !"
 
-    def bonjour(self):
+    def bonjour(self, moment):
+        if moment == MomentDeLaJournée.MATIN:
+            return "Good Morning"
+        if moment == MomentDeLaJournée.APRES_MIDI:
+            return "Good Afternoon"
+        if moment == MomentDeLaJournée.SOIR:
+            return "Good Evening"
+        if moment == MomentDeLaJournée.NUIT:
+            return "Good Night"
+
         return "Hello"
 
     def auRevoir(self):
--- test/utilities/detecteutPalindromeBuilder.py
+++ test/utilities/detecteutPalindromeBuilder.py
@@ -1,13 +1,19 @@
+from momentDeLaJournée import MomentDeLaJournée
 from src.detecteurPalindrome import DetecteurPalindrome
 from utilities.langueStub import LangueStub
 
 
 class DétecteurPalindromeBuilder:
     __langue = LangueStub()
+    __moment = MomentDeLaJournée.INCONNU
 
     def build(self):
-        return DetecteurPalindrome(self.__langue)
+        return DetecteurPalindrome(self.__langue, self.__moment)
 
     def ayantPourLangue(self, langue):
         self.__langue = langue
+        return self
+
+    def ayantPourMomentDeLaJournée(self, moment):
+        self.__moment = moment
         return self
